1f518201fa95f67a3ea01284be0d94086bde2d6f
Sahil <sahilismailnavar@gmail.com>
2025-05-23 16:25:30 +0530

    Added DB connection code

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 // port 
 const PORT = process.env.port || 3000;
 
 
 // routes 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hello")
 })
 
-// mongoose.connect(process.env.MONGODB_URL)
-
-// start server 
-app.listen(PORT, () => {
-    console.log(`server is running on port http://localhost:${PORT}`);
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("Database connected");
+    // start server 
+    app.listen(PORT, () => {
+        console.log(`server is running on port http://localhost:${PORT}`);
+    })
+}).catch(()=>{
+    console.log("Database connection failed");
 })
