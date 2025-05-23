7d5bf6763d1fbecd2f9d7df20925c1e300b7a02e
Sahil <sahilismailnavar@gmail.com>
2025-05-23 17:50:56 +0530

    Refactored code to MVC

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 const User = require("./models/User");
+const userRoutes = require("./routes/authRoutes");
 
 // port 
 const PORT = process.env.port || 3000;
@@ -13,42 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 // ejs 
 app.set('view engine', 'ejs');
 
-
-// routes 
-app.get("/auth/login", (req, res) => {
-    res.render("login");
-})
-
-app.get("/auth/register", (req, res) => {
-    res.render("register");
-})
-
-// main logic for the register 
-app.post("/auth/register", async (req, res) => {
-    // console.log(req.body);
-    const { username, email, password } = req.body;
-    try {
-        // if user exists 
-        const user = await User.findOne({ email });
-        if (user) {
-            return res.send("user already exists");
-        } else {
-            const newUser = new User({
-                username,
-                email,
-                password
-            });
-            // save the user    
-            await newUser.save();
-
-            // redirect to the login page 
-            res.redirect("/auth/login");
-        }
-    } catch (error) {
-        res.send(error);
-    }
-
-})
+//  routes
+app.use("/auth", userRoutes);
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
     console.log("Database connected");
